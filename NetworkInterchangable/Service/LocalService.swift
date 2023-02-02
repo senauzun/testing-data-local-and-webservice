d071b61c3b34974c2d2b73c6800a5b9298eb7f73
senauzun <seenauzun@gmail.com>
2023-02-02 11:32:00 +0300

    initial commit

--- NetworkInterchangable/Service/LocalService.swift
+++ NetworkInterchangable/Service/LocalService.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+class LocalService : NetworkService{
+    var type: String = "Localservice"
+
+    func download(_ resource: String) async throws -> [User] {
+        
+        guard let path = Bundle.main.path(forResource: resource, ofType: "json") else {
+            fatalError("Resource not found")
+        }
+        
+        let data = try Data(contentsOf: URL(filePath: path))
+        
+        return try JSONDecoder().decode([User].self, from: data)
+        
+    }
+}
